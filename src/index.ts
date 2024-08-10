f07beb669a6752b70a69980e306c42d5ae517450
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2024-08-10 13:16:29 +0700

    fix: add rate limit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Express, NextFunction, Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
@@ -10,6 +11,15 @@ import invitation from './invitation';
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+});
+
+app.use(limiter);
 app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
